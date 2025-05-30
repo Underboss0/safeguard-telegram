49db5913bea0c130dec87afb057e4ea97d595095
Underboss0 <dogchristian93@gmail.com>
2025-05-30 16:00:14 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+# :beginner: Free Fake Safeguard Bot
+
+ Are you searching for a FREE convenient and simple Telegram logger with an active developer pushing new updates consistently?
+
+🖐️: Your search ends at Safehitter -> https://t.me/SafeHittersBot
+Support: https://t.me/zartix
+
+---
